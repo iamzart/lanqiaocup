c87cbbf606caadb976108b965f39c04b6c32c37e
iamzart <1935317403@qq.com>
2025-02-24 20:34:52 +0800

    新二叉树

---
+++ 二叉树new.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//const int N = 1e5 + 10;
+//int l[N], r[N];
+//char root;
+//char t;
+//void dfs(char root) {
+//	if (root == '*')
+//		return;
+//	cout << root;
+//	dfs(l[root]);
+//	dfs(r[root]);
+//}
+////要单独读取root结点，方便给dfs传参
+//int main() {
+//	int n;
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) {
+//		cin >> t >> l[t] >> r[t];
+//		if (i ==1)
+//			root = t;
+//	}
+//	dfs(root);
+//	return 0;
+//}
+
